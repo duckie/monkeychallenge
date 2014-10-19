bc4e79da45de061746c688fe824bed38a5c2c1ec
Jean-Bernard Jansen <jeanbernard.jansen@gmail.com>
2014-10-19 22:32:49 +0200

    Simplifying gulp file

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,19 +1,10 @@
 var _ = require('underscore');
-var autoprefix = require('gulp-autoprefixer');
 var es = require('event-stream');
 var gulp = require('gulp');
-var karma = require('gulp-karma');
-var livereload = require('gulp-livereload');
-var replace = require('gulp-replace');
-var rjs = require('gulp-requirejs');
-var spawn = require('child_process').spawn;
-var uglify = require('gulp-uglify');
-var debug = require('gulp-debug');
-//var sass = require('gulp-ruby-sass');
-//var webdriver = require('gulp-protractor').webdriver_standalone;
-//var ngAnnotate = require('gulp-ng-annotate');
-//var protractor = require('gulp-protractor').protractor;
-//var deploy = require('gulp-gh-pages');
+var plugins = require("gulp-load-plugins")({
+    pattern: ['gulp-*', 'gulp.*', 'main-bower-files'],
+    replaceString: /\bgulp[\-.]/
+});
 
 var handleError = function (err) {
   console.log(err.name, ' in ', err.plugin, ': ', err.message);
@@ -21,53 +12,30 @@ var handleError = function (err) {
 };
 
 // Copy
-gulp.task('build', ['copy','js'], function () {
-  return es.concat();
-});
+gulp.task('build', ['copy','js']);
 
 // Copy
 gulp.task('copy', ['js'], function () {
   return es.concat(
     // update index.html to work when built
     gulp.src(['app/index.html'])
-      .pipe(gulp.dest('dist')),
+      .pipe(gulp.dest('dist'))
     // copy config-require
-    gulp.src(['app/scripts/config-require.js'])
-      .pipe(uglify().on('error', handleError))
-      .pipe(gulp.dest('dist/scripts')),
-    gulp.src(['source/vendor/**/*'])
-      .pipe(gulp.dest('dist/vendor')),
-    // minify requirejs
-    gulp.src(['build/vendor/requirejs/require.js'])
-      .pipe(uglify().on('error', handleError))
-      .pipe(gulp.dest('build/vendor/requirejs'))
+    , gulp.src(['app/scripts/**/*.js'])
+      .pipe(plugins.uglify().on('error', handleError))
+      .pipe(gulp.dest('dist/scripts'))
+    , gulp.src(['app/views/**/*.html']).pipe(gulp.dest('dist/views'))
+    , gulp.src(['app/styles/**/*.css']).pipe(gulp.dest('dist/styles'))
+    , gulp.src(['app/fonts/**/*']).pipe(gulp.dest('dist/fonts'))
   );
 });
 
 // JavaScript
 gulp.task('js', function () {
-  var configRequire = require('./app/scripts/config-require.js');
-  var configBuild = {
-    baseUrl: 'app',
-    insertRequire: ['scripts/main'],
-    name: 'scripts/main',
-    optimize: 'none',
-    out: 'allo.js',
-    wrap: true
-  };
-  var config = _(configRequire).extend(configBuild);
-  //var config = configRequire;
-
-  return gulp.src('app/scripts/main.js')
-    //.pipe(debug({verbose:true}))
-    .pipe(rjs(config).on('error', handleError))
-    //.pipe(ngAnnotate())
-    .pipe(uglify().on('error', handleError))
-    .pipe(gulp.dest('dist/scripts'));
-  //return rjs(config)
-    ////.pipe(ngAnnotate())
-    ////.pipe(uglify().on('error', handleError))
-    //.pipe(gulp.dest('dist/scripts')).on('error', handleError);
+  //return gulp.src(plugins.mainBowerFiles())
+  return gulp.src(['app/vendor/**/*.js'])
+    .pipe(plugins.filter(['**/*','!**/src/*','!**/test/**/*','!**/build/**/*']))
+    .pipe(gulp.dest('dist/vendor'));
 });
 
 // Karma
@@ -89,15 +57,10 @@ gulp.task('karma-ci', function () {
 
 // Watch
 gulp.task('watch', ['karma'], function () {
-  //gulp.run('sass');
   gulp.run('karma');
 
-  //gulp.watch('source/sass/**/*.scss', function () {
-    //gulp.run('sass');
-  //});
-
   // enable Livereload
-  livereload.listen();
+  plugins.livereload.listen();
   gulp.watch([
     'app/index.html',
     'app/scripts/**/*'
@@ -108,70 +71,4 @@ gulp.task('default', ['js', 'copy'], function () {
   gulp.run('karma-ci');
   gulp.run('protractor-ci');
 });
-
-// Sass
-//gulp.task('sass', function () {
-  //return gulp.src(['source/sass/*.scss', '!source/sass/_*.scss'])
-    //.pipe(sass({
-      //bundleExec: true,
-      //require: [
-        //'arkush/extensions',
-        //'sass-globbing'
-      //],
-      //style: 'compressed'
-    //}).on('error', handleError))
-    //.pipe(autoprefix().on('error', handleError))
-    //.pipe(gulp.dest('source/assets/css'));
-//});
-
-// Bump version
-//gulp.task('bump-version', function () {
-  //spawn('git', ['rev-parse', '--abbrev-ref', 'HEAD']).stdout.on('data', function (data) {
-//
-    //// Get current branch name
-    //var branch = data.toString();
-//
-    //// Verify we're on a release branch
-    //if (/^release\/.*/.test(branch)) {
-      //var newVersion = branch.split('/')[1].trim();
-//
-      //// Update client index.html
-      //gulp.src('./source/index.html')
-        //.pipe(replace(/(bust=v)(\d*\.?)*/g, '$1' + newVersion))
-        //.pipe(gulp.dest('./source'));
-//
-      //var updateJson = function (file) {
-        //gulp.src(file)
-          //.pipe(replace(/("version" *: *")([^"]*)(",)/g, '$1' + newVersion + '$3'))
-          //.pipe(gulp.dest('./'));
-      //};
-//
-      //updateJson('./bower.json');
-      //updateJson('./package.json');
-//
-      //console.log('Successfully bumped to ' + newVersion);
-    //} else {
-      //console.error('This task should be executed on a release branch!');
-    //}
-  //});
-//});
-
-// Protractor
-//gulp.task('protractor', function () {
-  //return gulp.src('source/js/**/*.e2e.js')
-    //.pipe(protractor({ configFile: 'p.conf.js' }));
-//});
-//
-//gulp.task('protractor-ci', function () {
-  //return gulp.src('source/js/**/*.e2e.js')
-    //.pipe(protractor({ configFile: 'p-compiled.conf.js' }));
-//});
-//
-//gulp.task('webdriver', webdriver);
-
-// Publish to GitHub Pages
-//gulp.task('gh-pages', ['js', 'copy'], function () {
-  //return gulp.src("./build/**/*")
-    //.pipe(deploy());
-//});
 
--- app/scripts/engine/monkeys.js
+++ app/scripts/engine/monkeys.js
@@ -1,7 +1,7 @@
 define(['underscore'],function(_) {
   return {
-    function roger() {
-      return 2;
-    }
+    //function roger() {
+      //return 2;
+    //}
   };  
 })

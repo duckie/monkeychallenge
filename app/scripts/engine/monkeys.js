59c9dc2053d4f08a72fdeec97470fad503437530
Jean-Bernard Jansen <jeanbernard.jansen@gmail.com>
2014-11-11 21:10:06 +0100

    Basic grid tested

--- app/scripts/engine/monkeys.js
+++ app/scripts/engine/monkeys.js
@@ -9,12 +9,42 @@ define(['underscore','rsvp'],function(_, RSVP) {
     count_until: 5,
 
     _play: function(posx, posy, resolve, reject) {
-      resolve(true); 
+      var pos = this.width*posx + posy;
+      var length = this.width*this.height;
+      var value_at_pos;
+
+      if (pos < 0 || length <= pos)
+        reject(new Error("Specified play position is out of range."));
+      
+      value_at_pos = this.values[pos];
+      if (value_at_pos === this.current_state.next_expected) {
+        if (value_at_pos === this.count_until) {
+          // Grid is finished
+          _.extend(this.current_state,{
+            current:value_at_pos,
+            next_expected:null,
+            finished:true
+          });
+        }
+        else {
+          // Step ok, but not finished yet
+          _.extend(this.current_state,{
+            current:value_at_pos,
+            next_expected:value_at_pos+1,
+          });
+        }
+      }
+      else if (false === this.current_state.finished) {
+        ++this.current_state.nb_failed_attempt;
+      }
+     
+      //console.log(this.current_state);
+      resolve(this.current_state);
       //reject(true);
     },
 
     play: function(posx, posy) {
-      self = this;
+      var self = this;
        return new RSVP.Promise(function(resolve, reject) {
          self._play(posx, posy, resolve, reject);
        });
@@ -23,7 +53,26 @@ define(['underscore','rsvp'],function(_, RSVP) {
 
   function GridPlayCtor(user_options) {
     var grid = Object.create(GridPlayBase);
+    var ordered_array = [];
+    var index;
+    var length;
+
     _.extend(grid, user_options);
+    grid.current = 0;
+    length = grid.width * grid.height;
+    for (index = 0; index < grid.count_until; ++index)
+      ordered_array[index] = index+1;
+    for (index = grid.count_until; index < length; ++index)
+      ordered_array[index] = 0;
+
+    grid.values = _.shuffle(ordered_array);
+    grid.current_state = {
+      current:0,
+      next_expected:1,
+      nb_failed_attempt:0,
+      finished:false
+    };
+
     return grid;
   };
 
--- test/mocha/test.spec.js
+++ test/mocha/test.spec.js
@@ -1,25 +1,57 @@
-define(['monkeys','chai-setup'], function(monkeys,chai) {
+define(['monkeys','chai-setup','rsvp'], function(monkeys,chai,RSVP) {
   'use strict';
   var expect = chai.expect;
 
   describe("Grid play model",function() {
 
-    var grid1 = monkeys.createSingleGridPlay({width:10});
+    beforeEach(function() {
+      this.grid1 = monkeys.createSingleGridPlay({width:10});
+      this.grid1.values.sort(function(a,b) {
+        if (a < b) return 1;
+        if (a > b) return -1;
+        return 0;
+      });
+    });
 
     it("Construction",function() { 
-      grid1.width.should.equal(10);
-      grid1.height.should.equal(5);
-      grid1.height = 6;
-      grid1.height.should.equal(6);
-      return grid1.play(0,0).should.eventually.equal(true);
+      this.grid1.width.should.equal(10);
+      this.grid1.height.should.equal(5);
+      this.grid1.current.should.equal(0);
+      this.grid1.count_until.should.equal(5);
+      this.grid1.values[0].should.equal(5);
     });
 
     it("Basic play",function() { 
-      grid1.height.should.equal(6);
-      //var grid1 = monkeys.createSingleGridPlay({width:10});
-      //grid1.width.should.equal(10);
-      //grid1.height.should.equal(5);
-      return grid1.play(0,0).should.eventually.equal(true);
+      return this.grid1.play(0,0).should.eventually.deep.include({
+        current:0,
+        next_expected:1,
+        nb_failed_attempt:1,
+        finished:false
+      });
+    });
+
+    it("Basic play 2",function() { 
+      var that = this;
+      return this.grid1.play(0,0)
+        .then(function() { return that.grid1.play(0,4); })
+        .should.eventually.deep.include({
+          current:1,
+          finished:false
+        });
+    });
+
+    it("Basic play 3",function() { 
+      var that = this;
+      return this.grid1.play(0,4)
+        .then(function() { return that.grid1.play(0,3); })
+        .then(function() { return that.grid1.play(0,2); })
+        .then(function() { return that.grid1.play(0,1); })
+        .then(function() { return that.grid1.play(0,0); })
+        .should.eventually.deep.include({
+          current:5,
+          next_expected:null,
+          finished:true
+        });
     });
   });
 });
